2cb25c79a98091a84acf258824383cfa9010630c
caslu0082008 <lucasaguiarbarbosaaguiar@gmail.com>
2025-06-01 00:36:34 -0300

    criaçao da função showItemList para renderizar na tela os itens do array

--- main.js
+++ main.js
@@ -13,4 +13,43 @@ function addItem() {
         name: itemName,
         checked: false
     }
-} 
+
+    items.push(item)
+
+    document.querySelector("#item").value = ""
+
+    showItemsList()
+}
+
+document.querySelector("#item").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addItem()
+    }
+})
+
+function showItemsList() {
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent = ""
+
+    items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
+
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+            <div class="item">
+                <div>
+                    <input type="checkbox" name="List" id="item-${index}" ${item.checked && "checked"}>
+
+                    <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+                        <img src="./assets/checked.svg" alt="checked">
+                    </div>
+
+                    <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+                </div>
+
+                <button onclick="removeItem('${item.name}')">
+                    <img src="./assets/trash-icon.svg" alt="trash icon">
+                </button>
+            </div>
+        `
+    })
+}
